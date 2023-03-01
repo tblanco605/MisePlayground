8b99df5353eb6310bf622d309de4ef155de25375
tblanco605 <123442530+tblanco605@users.noreply.github.com>
2023-02-28 22:06:40 -0800

    Delete playground directory

--- playground/mise/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MiseConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mise'
--- playground/mise/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('game', views.game, name='game'),
-    path('gamelvl2', views.gamelvl2, name='gamelvl2'),
-    
-]
